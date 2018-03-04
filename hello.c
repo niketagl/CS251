cdf736c0596907412070cde6fd87e4c30301d534
Niket Agrawal <niketagrawal99@gmail.com>
2018-03-04 23:46:23 +0530

    Step 19 of git assignment

--- hello.c
+++ hello.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 void microkernel_sendmsg(char *);
+void microkernel_getmsg(char *);
 
 void main(){
     printf("Helloworld!\n");
@@ -9,4 +10,7 @@ void main(){
 }
 void microkernel_sendmsg(char *a){
     printf("microkernel: %s\n", a);
+}
+void microkernel_getmsg(char *b){
+    //TODO: getmsg feature
 }
